ff976f483e24548d2943650772508c4aee96eb31
TMCKCM <31656229+TMCKCM@users.noreply.github.com>
2018-02-15 08:13:19 +0000

    Update 747.py

--- 747.py
+++ 747.py
@@ -31,7 +31,7 @@ def signup(emailchosen):
         s.cookies.clear()
         headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3334.0 Safari/537.36"}
         headers1={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3334.0 Safari/537.36", "content-type":"application/json; charset=UTF-8"}
-        payload={"email":"tommckenna4@googlemail.com","firstName":"Tom","lastName":"McKenna","gender":"M","datepicker":"7/23/1998","dateOfBirth":"1998-07-23","countryOfSite":"US","newsletterDomain":"United States","newsletterLanguage":"en","newsletterTypeId":"40000","source":"543452387","eventType":"adi_eCom_Basketball_adidas747","sendMail":"Y","consents":{"consent":[{"consentType":"AMF","consentValue":"N","consentVersion":"ADIUS_VER_1"}]}}
+        payload={"email":email,"firstName":NAME,"lastName":LASTNAME,"gender":"M","datepicker":"7/23/1998","dateOfBirth":"1998-07-23","countryOfSite":"US","newsletterDomain":"United States","newsletterLanguage":"en","newsletterTypeId":"40000","source":"543452387","eventType":"adi_eCom_Basketball_adidas747","sendMail":"Y","consents":{"consent":[{"consentType":"AMF","consentValue":"N","consentVersion":"ADIUS_VER_1"}]}}
         print("a*")
         a=s.get("http://www.adidas.com/us/747warehousest_signup?cm_sp=747-EVENT-PAGE-TICKETS-_-SIGN-UP", headers=headers)
         print("a")
@@ -50,4 +50,6 @@ print("747 SIGNUP SCRIPT")
 print("BY TMCKCM")
 print("----------------------")
 email=input("Gmail without '@gmail.com': ")
+NAME = input("Name: ")
+LASTNAME=input("Surname: ")
 signup(email)
